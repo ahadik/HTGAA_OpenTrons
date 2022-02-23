31a8f5095d722b928f8622654404c5cb36e2517f
Alexander Hadik <alex@alexhadik.com>
2022-02-23 09:19:58 -0500

    Add pipette_printer template. Modify printer demo to match steps in Notion.

--- protocols/pipette_printer/pipette_printer_template.py
+++ protocols/pipette_printer/pipette_printer_template.py
@@ -12,22 +12,28 @@ metadata = {
 # where to look for autocomplete suggestions
 def run(protocol: protocol_api.ProtocolContext):
 
+    # NOTE: Do not change anything in this section — we will assume the OpenTrons
+    # is set up according to this configuration
+    # ---------------------------------------------------------------------------
     # labware
     tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', '1')
     palette = protocol.load_labware('usascientific_12_reservoir_22ml', '2')
     canvas = protocol.load_labware('nest_96_wellplate_200ul_flat', '5')
 
+    color = {
+        'green': 'A1',
+        'blue': 'A2',
+        'red': 'A3',
+        'yellow': 'A4'
+    }
+
     # pipettes
     left_pipette = protocol.load_instrument(
          'p300_single', 'left', tip_racks=[tiprack])
 
-    # commands
-    left_pipette.pick_up_tip()
-    left_pipette.aspirate(100, palette['A1'])
-    left_pipette.dispense(100, canvas['A1'])
-    left_pipette.drop_tip()
-
-    left_pipette.pick_up_tip()
-    left_pipette.aspirate(100, palette['A2'])
-    left_pipette.dispense(100, canvas['A2'])
-    left_pipette.drop_tip()
+    # NOTE: Describe your commands below this line
+    # ---------------------------------------------------------------------------
+    # make sure you are pipetting FROM the palette TO the canvas
+    # use the color mpa for easy well reference:
+    # palette[color['green']] == palette['A1']
+    
